55e04a21116e89e1dd2246fea85a746a7a3ffbaa
Tjitse Efdé <tjitse@vendic.nl>
2021-09-03 10:44:26 +0200

    Clean up tests

--- __tests__/extract_task_ids.test.ts
+++ __tests__/extract_task_ids.test.ts
@@ -5,57 +5,54 @@ import path from "path";
 import * as fs from "fs";
 import {WebhookPayload} from "@actions/github/lib/interfaces";
 import nock from "nock";
+import {expect, test} from '@jest/globals'
 
-describe('Test pull request opened', () => {
-    it('extracts 4 task ids', async () => {
-        // Mocks
-        const infoMock = jest.spyOn(core, 'info')
-        const setOutputMock = jest.spyOn(core, 'setOutput')
-        const apiOutputPath = path.join(__dirname, 'api_output.json')
-        const payloadPath = path.join(__dirname, 'pull_request_context.json');
-        const payload = JSON.parse(fs.readFileSync(payloadPath, 'utf8'))
-        github.context.payload = payload as WebhookPayload
-        const apiOutput : JSON = JSON.parse(fs.readFileSync(apiOutputPath, 'utf-8'))
-        nock('https://api.github.com')
-            .persist()
-            .get('/repos/foo/bar/pulls/2/commits')
-            .reply(200, apiOutput)
+test('Extract 4 task ids after PR opened', async () => {
+    // Mocks
+    const infoMock = jest.spyOn(core, 'info')
+    const setOutputMock = jest.spyOn(core, 'setOutput')
+    const apiOutputPath = path.join(__dirname, 'api_output.json')
+    const payloadPath = path.join(__dirname, 'pull_request_context.json');
+    const payload = JSON.parse(fs.readFileSync(payloadPath, 'utf8'))
+    github.context.payload = payload as WebhookPayload
+    const apiOutput: JSON = JSON.parse(fs.readFileSync(apiOutputPath, 'utf-8'))
+    nock('https://api.github.com')
+        .persist()
+        .get('/repos/foo/bar/pulls/2/commits')
+        .reply(200, apiOutput)
 
-        await run()
+    await run()
 
-        // Assertions
-        expect(infoMock).toHaveBeenCalledWith('Found task id ABC-100')
-        expect(infoMock).toHaveBeenCalledWith('Found task id DEV-1234')
-        expect(infoMock).toHaveBeenCalledWith('Found task id ABC-123')
-        expect(infoMock).toHaveBeenCalledWith('Found task id ABC-999')
-        expect(setOutputMock).toHaveBeenCalledWith('task_ids', 'ABC-100\nDEV-1234\nABC-123\nABC-999')
-    })
+    // Assertions
+    expect(infoMock).toHaveBeenCalledWith('Found task id ABC-100')
+    expect(infoMock).toHaveBeenCalledWith('Found task id DEV-1234')
+    expect(infoMock).toHaveBeenCalledWith('Found task id ABC-123')
+    expect(infoMock).toHaveBeenCalledWith('Found task id ABC-999')
+    expect(setOutputMock).toHaveBeenCalledWith('task_ids', 'ABC-100\nDEV-1234\nABC-123\nABC-999')
 })
 
-describe('Test pull request review submitted', () => {
-    it('extracts 4 task ids', async () => {
-        // Mocks
-        const infoMock = jest.spyOn(core, 'info')
-        const setOutputMock = jest.spyOn(core, 'setOutput')
-        const apiOutputPath = path.join(__dirname, 'api_output.json')
-        const payloadPath = path.join(__dirname, 'pull_request_review_context.json');
-        const payload = JSON.parse(fs.readFileSync(payloadPath, 'utf8'))
-        github.context.payload = payload as WebhookPayload
-        const apiOutput : JSON = JSON.parse(fs.readFileSync(apiOutputPath, 'utf-8'))
-        nock('https://api.github.com')
-            .persist()
-            .get('/repos/foo/bar/pulls/2/commits')
-            .reply(200, apiOutput)
+test('Extract 4 task ids after PR review submitted', async () => {
+    // Mocks
+    const infoMock = jest.spyOn(core, 'info')
+    const setOutputMock = jest.spyOn(core, 'setOutput')
+    const apiOutputPath = path.join(__dirname, 'api_output.json')
+    const payloadPath = path.join(__dirname, 'pull_request_review_context.json');
+    const payload = JSON.parse(fs.readFileSync(payloadPath, 'utf8'))
+    github.context.payload = payload as WebhookPayload
+    const apiOutput: JSON = JSON.parse(fs.readFileSync(apiOutputPath, 'utf-8'))
+    nock('https://api.github.com')
+        .persist()
+        .get('/repos/foo/bar/pulls/2/commits')
+        .reply(200, apiOutput)
 
-        await run()
+    await run()
 
-        // Assertions
-        expect(infoMock).toHaveBeenCalledWith('Found task id ABC-100')
-        expect(infoMock).toHaveBeenCalledWith('Found task id DEV-1234')
-        expect(infoMock).toHaveBeenCalledWith('Found task id ABC-123')
-        expect(infoMock).toHaveBeenCalledWith('Found task id ABC-999')
-        expect(setOutputMock).toHaveBeenCalledWith('task_ids', 'ABC-100\nDEV-1234\nABC-123\nABC-999')
-    })
+    // Assertions
+    expect(infoMock).toHaveBeenCalledWith('Found task id ABC-100')
+    expect(infoMock).toHaveBeenCalledWith('Found task id DEV-1234')
+    expect(infoMock).toHaveBeenCalledWith('Found task id ABC-123')
+    expect(infoMock).toHaveBeenCalledWith('Found task id ABC-999')
+    expect(setOutputMock).toHaveBeenCalledWith('task_ids', 'ABC-100\nDEV-1234\nABC-123\nABC-999')
 })
 
 beforeEach(() => {
